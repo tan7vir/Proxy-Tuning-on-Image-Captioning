e6e553c5ba668a86130f002eaa6dad3297c02aed
Tan Vir <tanvirulislam.niloy@gmail.com>
2024-05-07 00:39:19 +0600

    This is for setting the Environment

---
+++ setup_environment.py
@@ -0,0 +1,6 @@
+def setup_environment() :
+    !pip install git+https://github.com/huggingface/transformers.git@main
+    !pip install -q datasets
+
+    from google.colab import drive
+    drive.mount('/content/drive')
